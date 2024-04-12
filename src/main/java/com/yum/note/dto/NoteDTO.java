31a42af941ad9dc889511246a169d040f0080c11
KCCI115\gladious <nuguri5165@naver.com>
2024-04-12 10:32:25 +0900

    공지사항 DTO 수정

--- src/main/java/com/yum/note/dto/NoteDTO.java
+++ src/main/java/com/yum/note/dto/NoteDTO.java
@@ -1,15 +1,29 @@
 package com.yum.note.dto;
 
 public class NoteDTO {
-
+	private int no;
 	private int noteIdx;
 	private String title;
 	private String content;
 	private String createDate;
 	private String writer;
+	private int totalCount = 0;		//게시글 전체 수
+	private int listCount = 10;		//한 페이지당 보여줄 게시글 수	
+	private int startIndex = 1;		//페이지 시작 게시글 번호
+	private int endIndex = 1;		//페이지 종료 게시글 번호
+	private int pagePerBlock = 10;	//하단 페이지 링크 개수
+	private int pageNum = 1;		//현재 페이지 번호
 	
 	public NoteDTO() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
 	}
 
 	public int getNoteIdx() {
@@ -50,6 +64,54 @@ public class NoteDTO {
 
 	public void setWriter(String writer) {
 		this.writer = writer;
+	}
+
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public int getListCount() {
+		return listCount;
+	}
+
+	public void setListCount(int listCount) {
+		this.listCount = listCount;
+	}
+
+	public int getPagePerBlock() {
+		return pagePerBlock;
+	}
+
+	public void setPagePerBlock(int pagePerBlock) {
+		this.pagePerBlock = pagePerBlock;
+	}
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+	
+	public int getStartIndex() {
+		return startIndex;
+	}
+
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+
+	public int getEndIndex() {
+		return endIndex;
+	}
+
+	public void setEndIndex(int startIndex) {
+		this.endIndex = startIndex + this.listCount - 1;
 	}
 
 	@Override
