af62f26c7ba9aa9177d87a51a3453960b380f4a8
jy <ji9626689@gmail.com>
2024-04-16 16:06:46 +0900

    필요없는것삭제

--- src/main/java/com/yum/member/controller/MyRecipeListController.java
+++ src/main/java/com/yum/member/controller/MyRecipeListController.java
@@ -31,7 +31,6 @@ public class MyRecipeListController extends HttpServlet {
 //		7. 마이페이지 - 내 글 확인 (레시피테이블 select id=특정값)
 		
 		// pagination 관련
-		RecipeDAO dao = new RecipeDAO();
 		PageNation paging = new PageNation();
 		RecipeDTO dto = new RecipeDTO();
 		if(request.getParameter("pageNum") != null) {
